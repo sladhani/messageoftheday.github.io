d73b18266c66ad95014b0e9a7c88253a5cc1705c
Sim <sladhani@syr.edu>
2019-05-09 13:35:07 -0400

    my first commit!

---
+++ script.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+	$('.frame').click(function(){
+		$('.cover').addClass('open');
+		$('.letter').addClass('pull');
+	});
+	$('.frameclose').click(function(){
+		$('.letter').addClass('pullback');
+		$('.cover').addClass('close');
+
+	});
+});
+$(document).ready(function(){
+	$('.frame2').click(function(){
+		$('.cover2').addClass('open2');
+		$('.letter2').addClass('pull2');
+	});
+	$('.frameclose2').click(function(){
+		$('.letter2').addClass('pullback2');
+		$('.cover2').addClass('close2');
+
+	});
+});
+$(document).ready(function(){
+	$('.frame3').click(function(){
+		$('.cover3').addClass('open3');
+		$('.letter3').addClass('pull3');
+	});
+	$('.frameclose3').click(function(){
+		$('.letter3').addClass('pullback3');
+		$('.cover3').addClass('close3');
+
+	});
+});
+
+$(document).ready(function(){
+	$('.frame4').click(function(){
+		$('.cover4').addClass('open4');
+		$('.letter4').addClass('pull4');
+	});
+	$('.frameclose4').click(function(){
+		$('.letter4').addClass('pullback4');
+		$('.cover4').addClass('close4');
+
+	});
+});
+
+$(document).ready(function(){
+	$('.frame5').click(function(){
+		$('.cover5').addClass('open5');
+		$('.letter5').addClass('pull5');
+	});
+	$('.frameclose5').click(function(){
+		$('.letter5').addClass('pullback5');
+		$('.cover5').addClass('close5');
+
+	});
+});
+
+$(document).ready(function(){
+	$('.frame6').click(function(){
+		$('.cover6').addClass('open6');
+		$('.letter6').addClass('pull6');
+	});
+	$('.frameclose6').click(function(){
+		$('.letter6').addClass('pullback6');
+		$('.cover6').addClass('close6');
+
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
